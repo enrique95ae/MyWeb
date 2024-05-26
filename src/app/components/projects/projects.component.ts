4d45cdf0f11f6a3e322ecc3d25622068132bea3a
Enrique Alonso Esposito <enrique95ae@gmail.com>
2024-05-25 22:23:19 -0700

    update

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -48,8 +48,8 @@ projects = [
       this.projects = [
           {
             imageUrl: 'assets/HWA_Screenshot.png',
-            title: 'Lorem Ipsum',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            title: 'Health Web App',
+            description: 'This is a smart health web application that helps users to track their health and fitness goals. It has a user-friendly interface and provides a seamless experience to the users. It is developed using Angular and integrates my custom made Python Flask API. Among the features are account creation, user authentication, meal creation and tracking, weight tracking, food database consults and more! '
           },
           {
             imageUrl: 'path_to_image2.jpg',
@@ -61,9 +61,9 @@ projects = [
     else if(selectedOption === 'backend'){
             this.projects = [
           {
-            imageUrl: '',
-            title: 'Lorem Ipsum',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            imageUrl: 'assets/HWA_API.png',
+            title: 'Health Web App API',
+            description: 'This is a custom made API for the Health Web App. It is developed using Python Flask and integrates with the Angular front-end. Among the features are user authentication, meal creation and tracking, weight tracking, food database consults and more! '
           }
         ];
     }
